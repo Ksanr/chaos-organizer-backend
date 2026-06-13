c1b71d2abcbf32b6574787dd4bb0a88a07551bcd
Aleksandr Lipunov <alipunov@mail.ru>
2026-06-13 02:59:54 +0000

    Commit for Clear server

--- app.js
+++ app.js
@@ -92,6 +92,7 @@ router.post('/messages/text', (ctx) => {
 });
 
 // Роут для загрузки файла (изображение, видео, аудио)
+/* закомментирую для очистки сервера
 router.post('/messages/file', upload.single('file'), (ctx) => {
   const { pinned = false, geo = null } = ctx.request.body;
   const file = ctx.file;
@@ -113,7 +114,7 @@ router.post('/messages/file', upload.single('file'), (ctx) => {
   messages.push(message);
   ctx.body = message;
 });
-
+*/
 // Роут для поиска сообщений (доп. функция)
 router.get('/messages/search', (ctx) => {
   const { q } = ctx.query;
